9a4b87b19ba7d7fd279d4ead570a08e952dd4b60
Epouvaantail <coutouzis.kilian@gmail.com>
2023-07-04 15:58:56 +0200

    update

--- argentbank/src/router/router.js
+++ argentbank/src/router/router.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.js";
 import SignIn from "../pages/SignIn.js";
+import User from "../pages/User.js";
 
 const Router = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </div>
   );
