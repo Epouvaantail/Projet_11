e3a4d02cf92e6d8529e12ee85fc5c7d2bdef6a83
Epouvaantail <coutouzis.kilian@gmail.com>
2023-07-19 14:51:45 +0200

    create store

--- argentbank/src/redux/store.js
+++ argentbank/src/redux/store.js
@@ -1,1 +1,9 @@
-// import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import logReducer from "./logSlice"
+
+const store = configureStore({
+    reducer: {
+      log: logReducer
+    },
+});
+export default store;
