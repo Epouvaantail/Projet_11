c6dcb33b3f408b66c0cf97606b5b2d6e327f8e4e
Epouvaantail <coutouzis.kilian@gmail.com>
2023-08-02 22:07:53 +0200

    protected route + rename routes

--- argentbank/src/router/router.js
+++ argentbank/src/router/router.js
@@ -3,14 +3,30 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.js";
 import SignIn from "../pages/SignIn.js";
 import User from "../pages/User.js";
+import { Navigate } from "react-router-dom";
+
+export const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/"/>
+  }
+  return children
+};
 
 const Router = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/user" element={<User />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route 
+          path="/profile" 
+          element={
+            <ProtectedRoute>
+              <User />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </div>
   );
