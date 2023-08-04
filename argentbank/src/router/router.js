82e0e5c38ddebc96ea4efb8e66729e9a5b3e1e76
Epouvaantail <coutouzis.kilian@gmail.com>
2023-08-04 10:01:16 +0200

    commentaire

--- argentbank/src/router/router.js
+++ argentbank/src/router/router.js
@@ -6,8 +6,11 @@ import User from "../pages/User.js";
 import { Navigate } from "react-router-dom";
 
 export const ProtectedRoute = ({ children }) => {
+  // récuperation du token dans le localStorage
   const token = localStorage.getItem("token");
+  // si il n'y a pas de token enregistré (pas d'utilisateur connecté)
   if (!token) {
+    // renvoie à la page d'accueil
     return <Navigate to="/"/>
   }
   return children
@@ -19,6 +22,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<SignIn />} />
+        {/* route protégée, afin que si personne est connécté on ne puisse pas avoir accès à la page profile */}
         <Route 
           path="/profile" 
           element={
